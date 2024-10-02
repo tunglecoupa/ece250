fb2c97ab4914ed2b2c002488dbb8ce2e371364c8
tung le <tunglevn@gmail.com>
2024-10-02 15:10:39 -0400

    Add code

---
+++ AVL/dsexceptions.h
@@ -0,0 +1,10 @@
+#ifndef _DSEXCEPTIONS_H_
+#define _DSEXCEPTIONS_H_
+
+class Underflow { };
+class Overflow { };
+class OutOfMemory { };
+class BadIterator { };
+class UnderflowException {};
+
+#endif#pragma once
